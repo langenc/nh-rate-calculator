ae81df982e91b7f986d804dde080ac39d2420276
Christoph Langen <christophlangen24@gmail.com>
2022-12-05 18:10:47 +0100

    Initial project setup

---
+++ src/main/java/com/nhratecalculator/nhratecalculator/NhRateCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.nhratecalculator.nhratecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhRateCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhRateCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhratecalculator/nhratecalculator/NhRateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhratecalculator.nhratecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhRateCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
